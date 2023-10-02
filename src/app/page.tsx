de3b9268aff6f57c374eca8e7b9b5463140a8fb1
Sinister-00 <sswapnil_be20@thapar.edu>
2023-10-03 03:16:31 +0530

    added cursor
    Signed-off-by: Sinister-00 <sswapnil_be20@thapar.edu>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import Image from "next/image";
-import { Poppins } from "next/font/google";
+import { useEffect, useState, useRef } from "react";
 import { assets } from "@/utils/asset-utils";
-import { useState, useEffect, use } from "react";
 import { type Framework, frameworks } from "@/utils/framework-util";
-import { FrameworkRotation } from "@/components/framework-rotation";
 import { cn } from "@/utils/tailwind-util";
+import { FrameworkRotation } from "@/components/framework-rotation";
+import { Poppins } from "next/font/google";
 import { CountdownTimer } from "@/components/countdown-timer";
+import { Cursor } from "@/components/cursor";
+
 const poppins = Poppins({
   weight: "700",
   subsets: ["latin"],
@@ -15,10 +17,9 @@ export default function Home() {
   const [currentFramework, setCurrentFramework] = useState<Framework>(
     frameworks[0]
   );
-  const [firstRender, setFirstRender] = useState(false);
-  useEffect(() => {
-    setFirstRender(true);
-  }, []);
+  const [showBackground, setShowBackground] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
   useEffect(() => {
     let currentIndex = 0;
     const rotateFrameworks = () => {
@@ -28,8 +29,14 @@ export default function Home() {
     const intervalId = setInterval(rotateFrameworks, 2000);
     return () => clearInterval(intervalId);
   }, []);
+
+  useEffect(() => {
+    setShowBackground(true);
+  }, []);
+
   return (
     <main>
+      {/* Background color */}
       <div
         className={cn(
           "fixed inset-0 transition-color delay-100 duration-700 opacity-25",
@@ -45,30 +52,35 @@ export default function Home() {
             "bg-neutral-300": currentFramework === "desktop",
           }
         )}
-      ></div>
-      <Image
-        width={1200}
-        height={1200}
-        src={assets.gradient}
-        alt="gradient"
-        role="presentation"
-        className="fixed inset-0 w-screen h-screen object-cover"
       />
+      {/* Grid */}
       <div
-        className="fixed inset-0 opacity-30"
         style={{
           backgroundSize: "30px",
           backgroundImage: `url(${assets.square})`,
         }}
-      ></div>
+        className="fixed inset-0 opacity-30"
+      />
+      {/* Gradient */}
+      <Image
+        width={1200}
+        height={1200}
+        role="presentation"
+        alt="gradient background"
+        className="fixed inset-0 w-screen h-screen object-cover"
+        src={assets.gradient}
+      />
+      {/* Reveal */}
       <div
         className={cn(
           "bg-black fixed inset-0 transition-opacity duration-1000",
-          !firstRender ? "opacity-100" : "opacity-0"
+          !showBackground ? "opacity-100" : "opacity-0"
         )}
       />
+      {/* Content */}
       <div className="max-w-7xl mt-20 mx-auto">
         <div className="flex flex-col items-center relative z-10">
+          {/* Heading */}
           <h1
             className={`text-7xl max-w-3xl text-center leading-snug mb-12 ${poppins.className}`}
           >
@@ -78,7 +90,7 @@ export default function Home() {
               src={assets.figma}
               width="50"
               height="50"
-            />{" "}
+            />
             to <FrameworkRotation currentFramework={currentFramework} /> will{" "}
             <span
               className={cn("transition-colors duration-200", {
@@ -97,31 +109,33 @@ export default function Home() {
             </span>{" "}
             be the same again
           </h1>
+          {/* Sub heading */}
           <p className="mb-8">
             <span className="text-gray-300">
-              Join the community for the event held by{" "}
+              Join us for an AI launch event by{" "}
             </span>
             <Image
-              alt="Event Logo"
+              alt="Builder.io logo"
               className="inline-block ml-1 -mt-1"
-              src={assets.builder}
-              height={20}
               width={100}
+              height={20}
+              src={assets.builder}
             />
             {" + "}
             <Image
-              alt="Event Logo"
-              className="inline-block ml-1 -mt-1"
-              src={assets.figmatwo}
-              height={20}
+              alt="Figma logo"
+              className="inline-block mx-1"
               width={55}
+              height={20}
+              src={assets.figmatwo}
             />
-            {" + "}
           </p>
+          {/* Claim ticket button */}
           <div className="mb-8">
             <button
+              ref={buttonRef}
               className={cn(
-                "text-black px-6 py-3 text-sm font-semibold rounded-md transition-colors duration-200",
+                "text-black px-6 py-3 rounded-md text-sm font-semibold transition-colors duration-200",
                 {
                   "bg-purple-300": currentFramework === "qwik",
                   "bg-sky-300": currentFramework === "safari",
@@ -135,13 +149,14 @@ export default function Home() {
                 }
               )}
             >
-              Clain Ticket
+              Claim Ticket
             </button>
           </div>
-
+          {/* Countdown timer */}
           <CountdownTimer currentFramework={currentFramework} />
         </div>
       </div>
+      <Cursor buttonRef={buttonRef} />
     </main>
   );
 }
